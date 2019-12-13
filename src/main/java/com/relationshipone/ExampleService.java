d1e37743e68d73d39bf86bf05ba1651a34a389f5
Benjamin Bublitz <benjamin.bublitz@relationshipone.com>
2019-12-13 14:53:57 -0600

    Added placeholder for coding example.

---
+++ src/main/java/com/relationshipone/ExampleService.java
@@ -0,0 +1,11 @@
+package com.relationshipone;
+
+/**
+ * Feel free to use this class or not.
+ */
+public class ExampleService {
+
+    public void run() {
+
+    }
+}
---
+++ src/test/java/com/relationshipone/CodeExample.java
@@ -0,0 +1,31 @@
+package com.relationshipone;
+
+import org.junit.Test;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * Given the unsecured endpoint
+ *      https://api.appcloud.relationshipone.com/appcloud/apps
+ * and the query parameter
+ *      eloquapackageId
+ * of value
+ *      153
+ * retrieve all app records (there are 3) and print to the logs (system console is fine) the
+ *      1. app name
+ *      2. documentation link (if available)
+ *
+ * Use the example() test as a run container to invoke your code.
+ * Put your code in src/main/java as though it would be production code.
+ * Make as many classes as you like. Use whatever libraries you like.
+ * Don't over-engineer, but separate responsibility into different classes where reasonable.
+ */
+public class CodeExample {
+
+    @Test
+    public void example() {
+        //Invoke your service(s) here. For example:
+        //new ExampleService().run();
+
+        //Don't worry about asserts. We're just abusing @Test to make the code run.
+    }
+}
